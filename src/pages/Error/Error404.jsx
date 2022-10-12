694d671241d816a229f64511f3ff10b94feeb054
B <77552838+Mrballer59@users.noreply.github.com>
2022-10-12 16:47:04 +0200

    error page fixed

--- src/pages/Error/Error404.jsx
+++ src/pages/Error/Error404.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../styles/index.css";
 function Error404() {
   return (
     <main className="main">
-      <div className="error404">Error 404</div>
+      <div className="error404">
+        <h1>Error 404 Sorry page not found</h1>
+        <NavLink to="/">
+          <button>Go back to the home page </button>
+        </NavLink>
+      </div>
     </main>
   );
 }
 export default Error404;
+
+{
+  /* <NavLink to="/">
+  <img src={mainLogo} alt="Logo of Argent Bank" />
+</NavLink>; */
+}
